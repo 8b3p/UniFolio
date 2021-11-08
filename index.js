bbc52d501d3e363addcf06bc66fc22719739948c
omair <omairdal@gmail.com>
2021-11-08 15:48:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+};
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: process.env.BINANCE_APIKEY,
+  APISECRET: process.env.BINANCE_APISECRET
+});
+
+const mongoDbUrl = process.env.MONGO_URL;
+mongoose.connect(mongoDbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('mongoose connected to mongoDB succesfully');
+  })
+  .catch(err => {
+    console.error('oh no, mongoose error');
+    console.log(err);
+  });
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  let wallet = 0;
+  let coins = ['SOL', 'SRM', 'LUNA', 'FTM', 'THETA', 'RAY', 'KSM', 'VET', 'LIT', 'TFUEL', 'PHA', 'COTI', 'RUNE', 'TLM'];
+  let balances;
+  try {
+    balances = await binance.balance();
+    console.log('got the balances');
+  } catch (err) {
+    console.log(err.statusMessage, err.statusCode)
+  }
+  for (let coin of coins) {
+    let coinusdt = `${coin}USDT`;
+    const ticker = await binance.prices(coinusdt);
+    let money = (parseFloat(balances[coin].available) + parseFloat(balances[coin].onOrder));
+    money = money * ticker[coinusdt]
+    wallet = wallet + money;
+    // console.log(wallet)
+  };
+  wallet += (parseFloat(balances.USDT.available) + parseFloat(balances.USDT.onOrder));
+  res.render('home', { balance: wallet, coins: coins })
+});
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`online on port ${port}`);
+});
