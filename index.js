327f5d3b0b5b6593445f607797ffb55423d3b3c4
omair <omairdal@gmail.com>
2022-04-02 15:07:27 +0300

    test3

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ passport.serializeUser(CoinUser.serializeUser());
 passport.deserializeUser(CoinUser.deserializeUser());
 
 app.use(function (req, res, next) {
-  console.log(req)
   console.log(req.url)
   console.log(req.baseUrl)
   for(let x = 0; x < 100; x++) {
